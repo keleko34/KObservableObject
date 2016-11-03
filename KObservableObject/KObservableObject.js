f2cd501a2d9ddc8fdd01361d0e1441f82e368d66
keleko34 <gregory.guidero@gmail.com>
2016-11-03 18:36:34 +0100

    add initial kbref

--- KObservableObject/KObservableObject.js
+++ KObservableObject/KObservableObject.js
@@ -318,7 +318,7 @@ define([],function(){
 
         Object.defineProperties(_obj,{
             __kbname:setDescriptor((name || ""),true),
-            __kbref:setDescriptor(((parent.__kbref || parent) || null),true),
+            __kbref:setDescriptor(((parent.__kbref || parent) || _obj),true),
             __kbscopeString:setDescriptor((scope || ""),true),
             __kbImmediateParent:setDescriptor((parent || null),true),
             add:setDescriptor(add),
